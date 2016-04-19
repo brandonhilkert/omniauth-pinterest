3485cc1d4ecc0a613558aabdfecdf1b17b3dbeee
Brandon Hilkert <brandonhilkert@gmail.com>
2016-04-19 11:33:06 -0400

    Add additional return fields and exlude query string from redirect_uri

--- lib/omniauth/strategies/pinterest.rb
+++ lib/omniauth/strategies/pinterest.rb
@@ -9,6 +9,8 @@ module OmniAuth
         :token_url => 'https://api.pinterest.com/v1/oauth/token'
       }
 
+      option :fields, "id,first_name,last_name,url,username,account_type,bio,counts,image[small,medium,large],created_at"
+
       def request_phase
         options[:scope] ||= 'read_public'
         options[:response_type] ||= 'code'
@@ -20,9 +22,18 @@ module OmniAuth
       info { raw_info }
 
       def raw_info
-        @raw_info ||= access_token.get('/v1/me/').parsed['data']
+        @raw_info ||= access_token.get('/v1/me/', params: { fields: options.fields }).parsed['data']
       end
 
+      def ssl?
+        true
+      end
+
+      private
+
+      def callback_url
+        options[:redirect_uri] || (full_host + script_name + callback_path)
+      end
     end
   end
 end
