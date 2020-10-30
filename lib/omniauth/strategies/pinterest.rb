4f3d4741c9bf74588218bfea5291bd94893bfdd7
Brandon Hilkert <brandonhilkert@gmail.com>
2020-10-30 15:58:30 -0700

    Update for v3 of pinner API

--- lib/omniauth/strategies/pinterest.rb
+++ lib/omniauth/strategies/pinterest.rb
@@ -4,15 +4,13 @@ module OmniAuth
   module Strategies
     class Pinterest < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        :site => 'https://api.pinterest.com/',
-        :authorize_url => 'https://api.pinterest.com/oauth/',
-        :token_url => 'https://api.pinterest.com/v1/oauth/token'
+        :site          => 'https://api.pinterest.com/',
+        :authorize_url => 'https://www.pinterest.com/oauth/',
+        :token_url     => 'https://api.pinterest.com/v3/oauth/access_token/'
       }
 
-      option :fields, "id,first_name,last_name,url,username,account_type,bio,counts,image[small,medium,large],created_at"
-
       def request_phase
-        options[:scope] ||= 'read_public'
+        options[:scope]         ||= 'read_users'
         options[:response_type] ||= 'code'
         super
       end
@@ -22,7 +20,7 @@ module OmniAuth
       info { raw_info }
 
       def raw_info
-        @raw_info ||= access_token.get('/v1/me/', params: { fields: options.fields }).parsed['data']
+        @raw_info ||= access_token.get('/v3/users/me/').parsed['data']
       end
 
       def ssl?
